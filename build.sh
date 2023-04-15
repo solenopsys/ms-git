5154ba338d5c42fc55bc4dc7d62f2ba779e0d78c
AlexStorm <info@alexstorm.ru>
2023-04-15 08:42:24 +0530

    ramove name

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.alexstorm.solenopsys.org/api/charts
+  curl --data-binary "@${FN}" http://helm.klogsolenopsys.org/api/charts
 }
 
-REGISTRY=registry.alexstorm.solenopsys.org
+REGISTRY=registry.klogsolenopsys.org
 NAME=alexstorm-git-wrapper
 ARCHS="linux/amd64,linux/arm64"
 VER=0.1.25
