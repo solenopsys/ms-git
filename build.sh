df6d7d5be4cb80a0e62d4ccacb306925a56bb28a
AlexStorm <info@alexstorm.ru>
2023-05-11 16:44:48 +0530

    fix domain

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.klogsolenopsys.org/api/charts
+  curl --data-binary "@${FN}" http://helm.solenopsys.org/api/charts
 }
 
-REGISTRY=registry.klogsolenopsys.org
+REGISTRY=registry.solenopsys.org
 NAME=alexstorm-git-wrapper
 ARCHS="linux/amd64,linux/arm64"
 VER=0.1.25
